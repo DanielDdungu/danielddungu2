3f32ecfdde062cf9ddf879016b1666819468d894
DanielDdungu <daddungu@outlook.com>
2022-01-20 20:25:34 +0100

    Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { SiDogecoin } from "react-icons/si";
 
