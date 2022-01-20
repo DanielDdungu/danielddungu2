8610452489d9baf7ee9ec506d307a0a92fc0fae9
DanielDdungu <daddungu@outlook.com>
2022-01-20 20:25:03 +0100

    Updated the footer anchor tags

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { SiDogecoin } from "react-icons/si";
 
@@ -18,17 +19,29 @@ function Footer() {
         <div className="sr-only sm:not-sr-only">
           <div class="grid grid-flow-col gap-4">
             <button className="btn btn-square btn-ghost">
-              <a href="#">
+              <a
+                href="https://github.com/DanielDdungu"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <FaGithub size={32} color />
               </a>
             </button>
             <button className="btn btn-square btn-ghost">
-              <a>
+              <a
+                href="https://www.linkedin.com/in/danielddungu/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <FaLinkedinIn size={25} color={"#0077b5"} />
               </a>
             </button>
             <button className="btn btn-square btn-ghost">
-              <a>
+              <a
+                href="https://twitter.com/daniel_ddungu"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <FaTwitter size={32} color={"#00acee"} className="md" />
               </a>
             </button>
