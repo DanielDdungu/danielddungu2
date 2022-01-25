25f9ce2852262841d113b3b6dee99e10915d5336
DanielDdungu <daddungu@outlook.com>
2022-01-25 09:30:19 +0100

    Portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,118 +5,142 @@ import estate from "../components/layout/assets/projects/estate.webp";
 import coming from "../components/layout/assets/projects/coming.webp";
 
 function Portfolio() {
-  return (
-    <div className="w-full bg-black rounded-box">
-      <section className="max-w-6xl mx-auto px-2 sm:px-6 lg:px-4 py-12 ">
-        <div className="text-center pb-12">
-          <h2 className="text-base font-bold text-white">
-            We have the best equipment in the market
-          </h2>
-          <h2 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-white">
-            Check a few of my awesome projects
-          </h2>
+    return (
+        <div className="w-full bg-black rounded-box">
+            <section className="max-w-6xl mx-auto px-2 sm:px-6 lg:px-4 py-12 ">
+                <div className="text-center pb-12">
+                    <h2 className="text-base font-bold text-white">
+                        Networthy Projects
+                    </h2>
+                    <h2 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-white">
+                        Check a few of my awesome projects
+                    </h2>
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
+                        <div>
+                            <img
+                                className="object-center object-cover h-auto w-full"
+                                src={estate}
+                                alt="project"
+                            />
+                        </div>
+                        <div className="text-center py-8 sm:py-6">
+                            <p className="text-xl text-white font-bold mb-2">
+                                Real Estate Application
+                            </p>
+                            <p className="text-base text-gray-400 font-normal ">
+                                Appartments for rent
+                            </p>
+                            <button class="btn glass mt-3 normal-case">
+                                Learn more
+                            </button>
+                        </div>
+                    </div>
+                    <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
+                        <div>
+                            <img
+                                className="object-center object-cover h-auto w-full"
+                                src={Shina}
+                                alt="project"
+                            />
+                        </div>
+                        <div className="text-center py-8 sm:py-6">
+                            <p className="text-xl text-white font-bold mb-2">
+                                Shina Fam Hardware
+                            </p>
+                            <p className="text-base text-gray-400 font-normal">
+                                E-commerce online hardware store
+                            </p>
+                            <button class="btn glass mt-3 normal-case">
+                                Learn more
+                            </button>
+                        </div>
+                    </div>
+                    <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
+                        <div>
+                            <img
+                                className="object-center object-cover h-auto w-full"
+                                src={Quiz}
+                                alt="projet"
+                            />
+                        </div>
+                        <div className="text-center py-8 sm:py-6">
+                            <p className="text-xl text-white font-bold mb-2">
+                                Quizz App
+                            </p>
+                            <p className="text-base text-gray-400 font-normal">
+                                React redux quiz appliation
+                            </p>
+                            <button class="btn glass mt-3 normal-case">
+                                Learn more
+                            </button>
+                        </div>
+                    </div>
+                    <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
+                        <div>
+                            <img
+                                className="object-center object-cover h-auto w-full"
+                                src={Github}
+                                alt="project"
+                            />
+                        </div>
+                        <div className="text-center py-8 sm:py-6">
+                            <p className="text-xl text-white font-bold mb-2">
+                                Github Finder
+                            </p>
+                            <p className="text-base text-gray-400 font-normal">
+                                Github Finder Appliation
+                            </p>
+                            <button class="btn glass mt-3 normal-case">
+                                Learn more
+                            </button>
+                        </div>
+                    </div>
+                    <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
+                        <div>
+                            <img
+                                className="object-center object-cover h-auto w-full"
+                                src={coming}
+                                alt="project"
+                            />
+                        </div>
+                        <div className="text-center py-8 sm:py-6">
+                            <p className="text-xl text-white font-bold mb-2">
+                                Coming Soon
+                            </p>
+                            <p className="text-base text-gray-400 font-normal">
+                                Coming Soon
+                            </p>
+                            <button class="btn glass mt-3 normal-case">
+                                Learn more
+                            </button>
+                        </div>
+                    </div>
+                    <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
+                        <div>
+                            <img
+                                className="object-center object-cover h-auto w-full"
+                                src={coming}
+                                alt="project"
+                            />
+                        </div>
+                        <div className="text-center py-8 sm:py-6">
+                            <p className="text-xl text-white font-bold mb-2">
+                                Coming Soon
+                            </p>
+                            <p className="text-base text-gray-400 font-normal">
+                                Coming Soon
+                            </p>
+                            <button class="btn glass mt-3 normal-case">
+                                Learn more
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                className="object-center object-cover h-auto w-full"
-                src={estate}
-                alt="project"
-              />
-            </div>
-            <div className="text-center py-8 sm:py-6">
-              <p className="text-xl text-white font-bold mb-2">
-                Real Estate Application
-              </p>
-              <p className="text-base text-gray-400 font-normal ">
-                Appartments for rent
-              </p>
-              <button class="btn glass mt-3 normal-case">Learn more</button>
-            </div>
-          </div>
-          <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                className="object-center object-cover h-auto w-full"
-                src={Shina}
-                alt="project"
-              />
-            </div>
-            <div className="text-center py-8 sm:py-6">
-              <p className="text-xl text-white font-bold mb-2">
-                Shina Fam Hardware
-              </p>
-              <p className="text-base text-gray-400 font-normal">
-                E-commerce online hardware store
-              </p>
-              <button class="btn glass mt-3 normal-case">Learn more</button>
-            </div>
-          </div>
-          <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                className="object-center object-cover h-auto w-full"
-                src={Quiz}
-                alt="projet"
-              />
-            </div>
-            <div className="text-center py-8 sm:py-6">
-              <p className="text-xl text-white font-bold mb-2">Quizz App</p>
-              <p className="text-base text-gray-400 font-normal">
-                React redux quiz appliation
-              </p>
-              <button class="btn glass mt-3 normal-case">Learn more</button>
-            </div>
-          </div>
-          <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                className="object-center object-cover h-auto w-full"
-                src={Github}
-                alt="project"
-              />
-            </div>
-            <div className="text-center py-8 sm:py-6">
-              <p className="text-xl text-white font-bold mb-2">Github Finder</p>
-              <p className="text-base text-gray-400 font-normal">
-                Github Finder Appliation
-              </p>
-              <button class="btn glass mt-3 normal-case">Learn more</button>
-            </div>
-          </div>
-          <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                className="object-center object-cover h-auto w-full"
-                src={coming}
-                alt="project"
-              />
-            </div>
-            <div className="text-center py-8 sm:py-6">
-              <p className="text-xl text-white font-bold mb-2">Coming Soon</p>
-              <p className="text-base text-gray-400 font-normal">Coming Soon</p>
-              <button class="btn glass mt-3 normal-case">Learn more</button>
-            </div>
-          </div>
-          <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                className="object-center object-cover h-auto w-full"
-                src={coming}
-                alt="project"
-              />
-            </div>
-            <div className="text-center py-8 sm:py-6">
-              <p className="text-xl text-white font-bold mb-2">Coming Soon</p>
-              <p className="text-base text-gray-400 font-normal">Coming Soon</p>
-              <button class="btn glass mt-3 normal-case">Learn more</button>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+    );
 }
 
 export default Portfolio;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,80 +4,83 @@ import digital from "../components/layout/assets/digital.webp";
 import ecommere from "../components/layout/assets/ecommere.webp";
 
 function Services() {
-  return (
-    <div className="conainer  bg-black rounded-box">
-      <div className="">
-        <p className="mb-5 text-2xl font-bold pt-6 pl-8 text-white">
-          MY SERVICES
-        </p>
-      </div>
-      <div className="p-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-3">
-        <div className="drop-shadow-2xl ... glass rounded-box hover:bg-base-100 ">
-          <div className="">
-            <img
-              className="object-scale-down h-50 w-98 ... rounded-t-2xl"
-              src={digital}
-              alt="..."
-            />
-          </div>
-          <div className="justify-center card-actions pt-6">
-            <FaSearchDollar size={30} style={{ fill: "#0ea5e9" }} />
-          </div>
-          <h4 className="text-center text-sky-400 uppercase card-title">
-            Digital Marketing
-          </h4>
-          <p className="text-center p-8 text-white">
-            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
-            in laying out print, graphic or web designs. The passage is
-            attributed to an unknown typesetter in the 15th century who is
-            thought to have scrambled parts
-          </p>
+    return (
+        <div className="conainer  bg-black rounded-box">
+            <div className="">
+                <p className="mb-5 text-2xl font-bold pt-6 pl-8 text-white">
+                    MY SERVICES
+                </p>
+            </div>
+            <div className="p-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-3">
+                <div className="drop-shadow-2xl ... glass rounded-box hover:bg-base-100 ">
+                    <div className="">
+                        <img
+                            className="object-scale-down h-50 w-98 ... rounded-t-2xl"
+                            src={digital}
+                            alt="..."
+                        />
+                    </div>
+                    <div className="justify-center card-actions pt-6">
+                        <FaSearchDollar size={30} style={{ fill: "#0ea5e9" }} />
+                    </div>
+                    <h4 className="text-center text-sky-400 uppercase card-title">
+                        Digital Marketing
+                    </h4>
+                    <p className="text-center p-8 text-white">
+                        Lorem ipsum, or lipsum as it is sometimes known, is
+                        dummy text used in laying out print, graphic or web
+                        designs. The passage is attributed to an unknown
+                        typesetter in the 15th century who is thought to have
+                        scrambled parts
+                    </p>
+                </div>
+                <div className="drop-shadow-2xl glass rounded-box hover:bg-base-100">
+                    <div className="">
+                        <img
+                            className="object-scale-down h-50 w-98 ... rounded-t-2xl"
+                            src="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
+                            alt="web dev"
+                        />
+                    </div>
+                    <div className="justify-center card-actions pt-6">
+                        <HiCode size={30} style={{ fill: "#0ea5e9" }} />
+                    </div>
+                    <h4 className="text-center text-sky-400 uppercase card-title ">
+                        Web Design & Development
+                    </h4>
+                    <p className="text-center text-white p-8 md:text-sm">
+                        Lorem ipsum, or lipsum as it is sometimes known, is
+                        dummy text used in laying out print, graphic or web
+                        designs. The passage is attributed to an unknown
+                        typesetter in the 15th century who is thought to have
+                        scrambled parts
+                    </p>
+                </div>
+                <div className="drop-shadow-2xl ... glass rounded-box hover:bg-base-100">
+                    <div className="">
+                        <img
+                            className="object-scale-down h-50 w-98 ... rounded-t-2xl"
+                            src={ecommere}
+                            alt="web dev"
+                        />
+                    </div>
+                    <div className="justify-center card-actions pt-6">
+                        <HiShoppingCart size={30} style={{ fill: "#0ea5e9" }} />
+                    </div>
+                    <h4 className="text-center text-sky-400 uppercase card-title">
+                        E-commerce
+                    </h4>
+                    <p className="text-center p-8 text-white">
+                        Lorem ipsum, or lipsum as it is sometimes known, is
+                        dummy text used in laying out print, graphic or web
+                        designs. The passage is attributed to an unknown
+                        typesetter in the 15th century who is thought to have
+                        scrambled parts
+                    </p>
+                </div>
+            </div>
         </div>
-        <div className="drop-shadow-2xl glass rounded-box hover:bg-base-100">
-          <div className="">
-            <img
-              className="object-scale-down h-50 w-98 ... rounded-t-2xl"
-              src="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
-              alt="web dev"
-            />
-          </div>
-          <div className="justify-center card-actions pt-6">
-            <HiCode size={30} style={{ fill: "#0ea5e9" }} />
-          </div>
-          <h4 className="text-center text-sky-400 uppercase card-title ">
-            Web Design & Development
-          </h4>
-          <p className="text-center text-white p-8 md:text-sm">
-            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
-            in laying out print, graphic or web designs. The passage is
-            attributed to an unknown typesetter in the 15th century who is
-            thought to have scrambled parts
-          </p>
-        </div>
-        <div className="drop-shadow-2xl ... glass rounded-box hover:bg-base-100">
-          <div className="">
-            <img
-              className="object-scale-down h-50 w-98 ... rounded-t-2xl"
-              src={ecommere}
-              alt="web dev"
-            />
-          </div>
-          <div className="justify-center card-actions pt-6">
-            <HiShoppingCart size={30} style={{ fill: "#0ea5e9" }} />
-          </div>
-          <h4 className="text-center text-sky-400 uppercase card-title">
-            E-commerce
-          </h4>
-          <p className="text-center p-8 text-white">
-            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
-            in laying out print, graphic or web designs. The passage is
-            attributed to an unknown typesetter in the 15th century who is
-            thought to have scrambled parts
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Services;
