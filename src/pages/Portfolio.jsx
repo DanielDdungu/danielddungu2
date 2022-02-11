3a909cc7302818f7362bce40b39c7a3882a5f7ad
DanielDdungu <daddungu@outlook.com>
2022-02-11 14:18:56 +0100

    updates

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,6 +4,9 @@ import Shina from "../components/layout/assets/projects/Shina.webp";
 import estate from "../components/layout/assets/projects/estate.webp";
 import coming from "../components/layout/assets/projects/coming.webp";
 import joomarexplolers from "../components/layout/assets/projects/joomarexplorers.jpg";
+import personalvehicle from "../components/layout/assets/projects/personalvehicle.jpg";
+import sorting from "../components/layout/assets/projects/sorting.jpg";
+import veronicadecorations from "../components/layout/assets/projects/veronicadecorations.jpg";
 import PageAnimation from "../components/layout/PageAnimation";
 import { motion } from "framer-motion";
 import { Helmet } from "react-helmet-async";
@@ -32,36 +35,34 @@ function Portfolio() {
                         </div>
                         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
                             {/**REAL EXTATE PROJECT */}
-                            <motion.div
-                                whileInView={{ scale: 1 }}
-                                whileTap={{ scale: 0.9 }}
-                                className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center"
-                            >
+                            <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
                                 <div>
                                     <img
                                         className="object-center object-cover h-auto w-full"
-                                        src={estate}
+                                        src={personalvehicle}
                                         alt="project"
                                     />
                                 </div>
                                 <div className="text-center py-8 sm:py-6">
                                     <p className="text-xl text-white font-bold mb-2">
-                                        Real Estate Application
+                                        Smart Watch Solutions
                                     </p>
                                     <p className="text-base text-gray-400 font-normal ">
-                                        Appartments for rent
+                                        Provider of fleet management services
                                     </p>
                                     <button class="btn glass mt-3 normal-case">
-                                        Learn more
+                                        <a
+                                            href="https://smartwatchsolutions.com//"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            Learn more
+                                        </a>
                                     </button>
                                 </div>
-                            </motion.div>
+                            </div>
                             {/**SHINA FAM APPLICATION */}
-                            <motion.div
-                                whileInView={{ scale: 1 }}
-                                whileTap={{ scale: 0.9 }}
-                                className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center"
-                            >
+                            <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
                                 <div>
                                     <img
                                         className="object-center object-cover h-auto w-full"
@@ -77,41 +78,45 @@ function Portfolio() {
                                         E-commerce online hardware store
                                     </p>
                                     <button class="btn glass mt-3 normal-case">
-                                        Learn more
+                                        <a
+                                            href="http://20667alumno3.com/"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            Learn more
+                                        </a>
                                     </button>
                                 </div>
-                            </motion.div>
+                            </div>
                             {/**QUIZ APPLICATION */}
-                            <motion.div
-                                whileInView={{ scale: 1 }}
-                                whileTap={{ scale: 0.9 }}
-                                className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center"
-                            >
+                            <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
                                 <div>
                                     <img
                                         className="object-center object-cover h-auto w-full"
-                                        src={Quiz}
+                                        src={sorting}
                                         alt="projet"
                                     />
                                 </div>
                                 <div className="text-center py-8 sm:py-6">
                                     <p className="text-xl text-white font-bold mb-2">
-                                        Quizz App
+                                        Sorting Hut Quizz App
                                     </p>
                                     <p className="text-base text-gray-400 font-normal">
-                                        React redux quiz appliation
+                                        Harry Potter sorting hat with React js
                                     </p>
                                     <button class="btn glass mt-3 normal-case">
-                                        Learn more
+                                        <a
+                                            href="https://vercel.com/danielddungu/sorting-hat-application"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            Learn more
+                                        </a>
                                     </button>
                                 </div>
-                            </motion.div>
+                            </div>
                             {/**GITHUB FINDER */}
-                            <motion.div
-                                whileInView={{ scale: 1 }}
-                                whileTap={{ scale: 0.9 }}
-                                className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center"
-                            >
+                            <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
                                 <div>
                                     <img
                                         className="object-center object-cover h-auto w-full"
@@ -127,16 +132,18 @@ function Portfolio() {
                                         Github Finder Appliation
                                     </p>
                                     <button class="btn glass mt-3 normal-case">
-                                        Learn more
+                                        <a
+                                            href="https://github-finder-mauve.vercel.app/"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            Learn more
+                                        </a>
                                     </button>
                                 </div>
-                            </motion.div>
+                            </div>
                             {/**COMING SOON */}
-                            <motion.div
-                                whileInView={{ scale: 1 }}
-                                whileTap={{ scale: 0.9 }}
-                                className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center"
-                            >
+                            <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
                                 <div>
                                     <img
                                         className="object-center object-cover h-auto w-full"
@@ -146,22 +153,104 @@ function Portfolio() {
                                 </div>
                                 <div className="text-center py-8 sm:py-6">
                                     <p className="text-xl text-white font-bold mb-2">
-                                        Coming Soon
+                                        Joomar Explorers
                                     </p>
                                     <p className="text-base text-gray-400 font-normal">
-                                        Coming Soon
+                                        Joomar Explorers Tourist Website
                                     </p>
                                     <button class="btn glass mt-3 normal-case">
-                                        Learn more
+                                        <a
+                                            href="https://www.jomaarexplores.com/"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            Learn more
+                                        </a>
                                     </button>
                                 </div>
-                            </motion.div>
+                            </div>
                             {/**COMING SOON */}
-                            <motion.div
-                                whileInView={{ scale: 1 }}
-                                whileTap={{ scale: 0.9 }}
-                                className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center"
-                            >
+                            <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
+                                <div>
+                                    <img
+                                        className="object-center object-cover h-auto w-full"
+                                        src={estate}
+                                        alt="project"
+                                    />
+                                </div>
+                                <div className="text-center py-8 sm:py-6">
+                                    <p className="text-xl text-white font-bold mb-2">
+                                        Real Estate Application
+                                    </p>
+                                    <p className="text-base text-gray-400 font-normal">
+                                        React Js Real estate application
+                                    </p>
+                                    <button class="btn glass mt-3 normal-case">
+                                        <a
+                                            href="https://smartwatchsolutions.com//"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            Learn more
+                                        </a>
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
+                                <div>
+                                    <img
+                                        className="object-center object-cover h-auto w-full"
+                                        src={Quiz}
+                                        alt="project"
+                                    />
+                                </div>
+                                <div className="text-center py-8 sm:py-6">
+                                    <p className="text-xl text-white font-bold mb-2">
+                                        Github Finder
+                                    </p>
+                                    <p className="text-base text-gray-400 font-normal">
+                                        Github Finder Appliation
+                                    </p>
+                                    <button class="btn glass mt-3 normal-case">
+                                        <a
+                                            href="https://smartwatchsolutions.com//"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            Learn more
+                                        </a>
+                                    </button>
+                                </div>
+                            </div>
+                            {/**COMING SOON */}
+                            <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
+                                <div>
+                                    <img
+                                        className="object-center object-cover h-auto w-full"
+                                        src={veronicadecorations}
+                                        alt="project"
+                                    />
+                                </div>
+                                <div className="text-center py-8 sm:py-6">
+                                    <p className="text-xl text-white font-bold mb-2">
+                                        Veronica Decorations
+                                    </p>
+                                    <p className="text-base text-gray-400 font-normal">
+                                        Events decoration e-commerce website
+                                    </p>
+                                    <button class="btn glass mt-3 normal-case">
+                                        <a
+                                            href="https://smartwatchsolutions.com//"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            Learn more
+                                        </a>
+                                    </button>
+                                </div>
+                            </div>
+                            {/**COMING SOON */}
+                            <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
                                 <div>
                                     <img
                                         className="object-center object-cover h-auto w-full"
@@ -177,10 +266,16 @@ function Portfolio() {
                                         Coming Soon
                                     </p>
                                     <button class="btn glass mt-3 normal-case">
-                                        Learn more
+                                        <a
+                                            href="https://smartwatchsolutions.com//"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            Learn more
+                                        </a>
                                     </button>
                                 </div>
-                            </motion.div>
+                            </div>
                         </div>
                     </section>
                 </div>
