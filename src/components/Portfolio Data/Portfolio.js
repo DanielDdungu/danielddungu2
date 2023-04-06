251ab02d440c0f9ef20ce39b5086995537f30894
DanielDdungu <daddungu@outlook.com>
2023-04-06 10:49:21 +0200

    Added a portifolio project

---
+++ src/components/Portfolio Data/Portfolio.js
@@ -0,0 +1,49 @@
+export const dataList = [
+    {
+        category: "Web Design",
+        id: 1,
+        title: "Translinguo Global",
+        image: "../components/layout/assets/projects/Translinguo.png",
+        description: "Translinguo Global® is a translation agency for companies and individuals that optimises its professional" +
+            " translation services through technological support and experience in the sector.",
+        link: "https://translinguoglobal.com/"
+    },
+    {
+        category: "Web Design",
+        id: 2,
+        title: "Solusat Official Technical Service",
+        image: "../components/layout/assets/projects/SOLUSAT.png",
+        description: "They offer repair, annual overhaul and maintenance services in Madrid, both for individuals and companies.",
+        link: "https://www.solusat.es/"
+    },
+    {
+        category: "Web Design",
+        id: 3,
+        title: "Smart Watch Solutions",
+        image: "../components/layout/assets/projects/personalvehicle.jpg",
+        description: "<strong>Smart Watch Solutions</strong> Is East Africa´s leading provider of fleet mobile asset management " +
+            "solutions. Established in 2011 as direct response to the emerging trends in dynamic Mobile Resource Management, " +
+            "Smartwatch Solutions has proven track record in delivering successful Software-as-a service fleet management," +
+            " workshop management and end-to-end I.o.T SMART solutions.",
+        link: "https://smartwatchsolutions.com/"
+    },
+    {
+        category: "Web Design",
+        id: 4,
+        title: "Shina Fam Hardware",
+        image: "../components/layout/assets/projects/Shina.webp",
+        description: "Online hardware tools store",
+        link: "https://shinafam.ug"
+    },
+    {
+        category: "Web Design",
+        id: 5,
+        title: "Jomaar Explorers",
+        image: "../components/layout/assets/projects/joomarexplorers.jpg",
+        description: "We are proud to operate in Uganda, one of East Africa’s fastest growing and most exciting, " +
+            "diversified safari destinations offering year round opportunities for tourism. Uganda offers numerous " +
+            "opportunities to experience the safari of a lifetime;d",
+        link: "https://www.jomaarexplores.com"
+    },
+
+];
